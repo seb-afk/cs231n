ff7abcadd171e04a66f506357a4757f9f9781456
Sebastian Bertoli <sebastian.bertoli@fraunhofer.it>
2018-05-29 11:30:28 +0200

    update gradient only if margin > 0

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -34,9 +34,8 @@ def svm_loss_naive(W, X, y, reg):
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
         loss += margin
-        # print(dW[:,j].shape, X[i].shape)
-      dW[:,j] += X[i] 
-      dW[:,y[i]] -= X[i] 
+        dW[:,j] += X[i] 
+        dW[:,y[i]] -= X[i] 
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
